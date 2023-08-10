19cc0832d625ae04ab5bb205188bc3b3ade044fc
Caio Nascimento <caionsantos2000@gmail.com>
2023-08-10 12:38:12 -0300

    feat: post factory and seed

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
      * Seed the application's database.
@@ -13,10 +13,5 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         \App\Models\User::factory(10)->create();
-
-        \App\Models\User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
     }
 }
