b4c3a2643e3e5709fd153a2c85e7150629b2a81a
Caio Nascimento <caionsantos2000@gmail.com>
2023-08-11 13:50:41 -0300

    chore: add purifier lib to sanitize post body

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,10 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Mews\Purifier\Casts\CleanHtml;
+use Mews\Purifier\Casts\CleanHtmlInput;
+use Mews\Purifier\Casts\CleanHtmlOutput;
 
 class Post extends Model
 {
     use HasFactory;
+    protected $casts = [
+        'body'             => CleanHtml::class, // cleans both when getting and setting the value
+        //'body'             => CleanHtmlInput::class, // cleans when setting the value
+        //'body'             => CleanHtmlOutput::class, // cleans when getting the value
+    ];
+
+
     /**
      * The attributes that are mass assignable.
      *
